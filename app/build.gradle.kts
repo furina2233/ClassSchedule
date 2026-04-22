984308484bb5d23d7ab2b84d987b1bd36cb085c1
LFF <lff18276496706@gmail.com>
2026-04-22 16:58:52 +0800

    1.3.1版本，修改了桌面小部件的更新频率

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "1.3"
+        versionName = "1.3.1"
         buildConfigField("long", "BUILD_TIME", "${System.currentTimeMillis()}L")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
